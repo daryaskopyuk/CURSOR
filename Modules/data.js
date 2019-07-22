5209b24390aa16770b549063f6fdbeb2d4c13b90
daryaskopyuk <daryaskopyuk@gmail.com>
2019-07-22 11:51:43 +0300

    master | modules

---
+++ Modules/data.js
@@ -0,0 +1,32 @@
+export const pi = 3.14;
+
+
+const random = 31;
+const max = 100;
+const name = 'Darya';
+
+export { random, max, name as user }
+
+
+export function sum(a, b) {
+  return a + b;
+}
+
+export class Book {
+  constructor(title) {
+    this.title= title;
+  }
+}
+
+
+const defaultValue = 'Cursor';
+
+export default defaultValue;
+
+const testValue = 'Test'
+
+export default testValue;
+
+
+
+
---
+++ Modules/main.js
@@ -0,0 +1,23 @@
+import { pi, random, max as maximumNumber, user, sum, Book } from './data.js';
+import * as data from './data.js';
+import defaultValue from './data.js';
+
+console.log(`Pi is ${pi}, random is ${random}`);
+
+console.log(`Maximum number is ${maximumNumber}`);
+
+console.log(`Current user is ${user}`);
+
+console.log(`Sum is ${sum(5, 4)}`);
+
+
+const book = new Book('You don\'t know JS');
+
+console.log(book);
+
+// export function() { console.log('Error'); }; // error
+
+console.log(data);
+
+
+console.log(`Default val is ${defaultValue}`);
